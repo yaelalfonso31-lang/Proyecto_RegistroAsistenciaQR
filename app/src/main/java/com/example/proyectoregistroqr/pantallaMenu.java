e0c48e6f146018cf0573d9bcf7ad0f0098111419
Carmona21 <eduardoect21@gmail.com>
2026-05-05 17:38:52 -0600

    Creacion de la pantalla de escaneo de qr

--- app/src/main/java/com/example/proyectoregistroqr/pantallaMenu.java
+++ app/src/main/java/com/example/proyectoregistroqr/pantallaMenu.java
@@ -1,6 +1,9 @@
 package com.example.proyectoregistroqr;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +22,16 @@ public class pantallaMenu extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button btnEscaner = findViewById(R.id.btnEscanerQR);
+
+        btnEscaner.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(pantallaMenu.this, pantallaQR.class);
+                startActivity(intent);
+            }
         });
     }
 }
